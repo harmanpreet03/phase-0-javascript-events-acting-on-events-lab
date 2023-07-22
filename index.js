2960bc32806e2f426f711ac67ba3ac3502fcb977
harmanpreet ghotra <harmanpreetghotra17@gmail.com>
2023-07-22 16:31:17 -0400

    frames moving

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+dodger.style.bottom = "0px"
+dodger.style.left = "0px"
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+  function moveDodgerRight() {
+    const rightNumbers = dodger.style.left.replace("px", "");
+    const right = parseInt(rightNumbers, 10);
+  
+    const maxWidth = window.innerWidth; // Get the maximum width of the screen
+  
+    if (right < maxWidth - 40) { // 40 is just an arbitrary value to prevent the Dodger from going off the screen completely
+      dodger.style.left = `${right + 1}px`;
+    }
+  }
